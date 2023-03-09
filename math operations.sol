0c195e5e92205f414bfb88a4c87f9a44b2e78217
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-03-09 12:00:39 +0530

    Update math operations.sol

--- math operations.sol
+++ math operations.sol
@@ -1,18 +1,18 @@
 //0xc0a05C96e43D228189e0e1AbCFeb55EE8f6Ddf0f
 
-pragma solidity ^0.5.1;
+pragma solidity^0.8.1;
 
 contract math{
-    function add(uint x, uint y) external pure returns(uint){
+    function add(uint256 x, uint256 y) external pure returns(uint256){
         return x+y;
     }
-    function sub(uint x, uint y) external pure returns(uint){
+    function sub(uint256 x, uint256 y) external pure returns(uint256){
         return x-y;
     }
-    function mul(uint x, uint y) external pure returns(uint){
+    function mul(uint256 x, uint256 y) external pure returns(uint256){
         return x*y;
     }
-    function div(uint x, uint y) external pure returns(uint){
+    function div(uint256 x, uint256 y) external pure returns(uint256){
         require(y !=0, "y must be non-zero");
         return x/y;
     }
