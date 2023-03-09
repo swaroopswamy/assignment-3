727f52d295162928f45422f16cc36a12e553faba
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-03-09 11:57:57 +0530

    Update counter.sol

--- counter.sol
+++ counter.sol
@@ -1,9 +1,9 @@
 //0x646aa2BaE79E67239E9Cd355414731a7b799CaC6
 
-pragma solidity^0.5.1;
+pragma solidity^0.8.1;
 
 contract statevariable{
-    uint public _count;
+    uint256 public _count;
 
     function increment() public {
         _count=_count + 1;
@@ -11,7 +11,7 @@ contract statevariable{
     function reset() public {
         _count= 0;
     }
-    function getCount() public view returns(uint){
+    function getCount() public view returns(uint256){
         return _count;
     }
 }
