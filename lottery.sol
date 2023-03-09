9ea20cd66ec875f88abefc37f384b2c691ab031d
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-03-09 11:58:52 +0530

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -1,15 +1,15 @@
 //0x1076a99DBfaEe5f85241fDac90FA1993A250ceb5
 
-pragma solidity ^0.5.1;
+pragma solidity^0.8.1;
  
 contract lottery{
-    uint public winner;
+    uint256 public winner;
 
     function play() public {
-        uint rand=uint(keccak256(abi.encode(block.timestamp,block.difficulty,address(msg.sender))));
+        uint256 rand=uint256(keccak256(abi.encode(block.timestamp,block.difficulty,address(msg.sender))));
         winner = rand % 2;
     }
-        function getwinner() public view returns(uint){
+        function getwinner() public view returns(uint256){
             return winner;
         }
 }
