30004d90235274c7c1ef5d344d44af3dccecc0d0
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-03-28 20:14:56 +0530

    Update math operations.sol

--- math operations.sol
+++ math operations.sol
@@ -1,20 +1,36 @@
-//0xc0a05C96e43D228189e0e1AbCFeb55EE8f6Ddf0f
+//0xf8e81D47203A594245E36C48e151709F0C19fBe8
 
-pragma solidity^0.8.1;
+ pragma solidity ^0.8.1;
 
-contract math{
-    function add(uint256 x, uint256 y) external pure returns(uint256){
-        return x+y;
+library safeMath {
+    function add(int256 x,int256 y) internal pure returns(int256){
+        int256 z= x + y;
+        return z;
     }
-    function sub(uint256 x, uint256 y) external pure returns(uint256){
-        return x-y;
+    function sub(int256 x,int256 y) internal pure returns(int256){
+        int256 z= x - y;
+        return z;
     }
-    function mul(uint256 x, uint256 y) external pure returns(uint256){
-        return x*y;
+    function mul(int256 x,int256 y) internal pure returns(int256){
+        int256 z= x * y;
+        return z;
     }
-    function div(uint256 x, uint256 y) external pure returns(uint256){
-        require(y !=0, "y must be non-zero");
-        return x/y;
+    function div(int256 x,int256 y) internal pure returns(int256){
+        int256 z= x / y;
+        assert(y != 0);
+        return z;
+    }
+
+    function testadd(int256 x, int256 y) external pure returns(int256){
+        return safeMath.add(x,y);
+    }
+    function testsub(int256 x, int256 y) external pure returns(int256){
+        return safeMath.sub(x,y);
+    }
+    function testmul(int256 x, int256 y) external pure returns(int256){
+        return safeMath.mul(x,y);
+    }
+    function testdiv(int256 x, int256 y) external pure returns(int256){
+        return safeMath.div(x,y);
     }
 }
-
